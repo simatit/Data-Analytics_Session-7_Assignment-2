a137d8f3739eae5dc831e15f92010e3df3322482
SIMATIT MUNI <simatitcse@gmail.com>
2018-09-13 15:33:39 +0530

    Data-Analytics_Session-7_Assignment-2

---
+++ 7.2.R
@@ -0,0 +1,28 @@
+# 1. Write a program to create barplots for all the categorical columns in mtcars.
+counts<- table(mtcars$carb)
+barplot(counts ,main ="carb",xlab="carb",ylab = "counts",col="pink")
+counts<- table(mtcars$cyl)
+barplot(counts ,main ="cyl",xlab="cyl",ylab = "counts",col="red")
+counts<- table(mtcars$vs)
+barplot(counts ,main ="vs",xlab="vs",ylab = "counts",col="blue")
+counts<- table(mtcars$am)
+barplot(counts ,main ="am",xlab="am",ylab = "counts",col="yellow")
+counts<- table(mtcars$gear)
+barplot(counts ,main ="gear",xlab="gear",ylab = "counts",col="green")
+
+# 2. Create a scatterplot matrix by gear types in mtcars dataset.
+# Kindly expand the plot area so that below code works smoothly
+par(mfrow = c(3,4))
+plot(mtcars$mpg,mtcars$gear)
+plot(mtcars$cyl,mtcars$gear)
+plot(mtcars$disp,mtcars$gear)
+plot(mtcars$hp,mtcars$gear)
+plot(mtcars$drat,mtcars$gear)
+plot(mtcars$wt,mtcars$gear)
+plot(mtcars$qsec,mtcars$gear)
+plot(mtcars$vs,mtcars$gear)
+plot(mtcars$carb,mtcars$gear)
+plot(mtcars$am,mtcars$gear)
+
+# 3. Write a program to create a plot density by class variable.
+mtcars %>%  gather() %>%ggplot(aes(value)) +facet_wrap(~ key,scales = "free") +stat_density()
